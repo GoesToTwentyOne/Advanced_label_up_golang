56c30c271dbaa438422ea3574de5bfd56ed38663
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-03-07 00:02:18 +0600

    net package read 02

---
+++ 14_read02/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			// continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	//convert the request byte string
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		//string convert to the string
+		ln := scanner.Text()
+		fmt.Println(ln)
+	}
+	defer conn.Close()
+
+	// we never get here
+	// we have an open stream connection
+	// how does the above reader know when it's done?
+	fmt.Println("Code got here.")
+}
