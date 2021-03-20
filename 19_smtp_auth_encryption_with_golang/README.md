a8903b9e791930fc30c95251421250be140eb652
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-03-20 17:56:26 +0600

    ssl encription

---
+++ 19_smtp_auth_encryption_with_golang/README.md
@@ -0,0 +1,2 @@
+# test02withedx
+openssl s_client -connect smtp.gmail.com:587 -quiet -starttls smtp
--- 19_smtp_auth_encryption_with_golang/main.go
+++ 19_smtp_auth_encryption_with_golang/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	resp := []byte("\x00" + "nihadhossain22@gmail.com" + "\x00" + "AlexGoot210@")
+	resp := []byte("\x00" + "nihadhossain22@gmail.com" + "\x00" + "ladyGaga")
 	fmt.Println(string(resp), resp)
 	// smtpAuthENC := base64.StdEncoding.EncodeToString([]byte(resp))
 	smtpAuthENC := base64.StdEncoding.EncodeToString(resp)
