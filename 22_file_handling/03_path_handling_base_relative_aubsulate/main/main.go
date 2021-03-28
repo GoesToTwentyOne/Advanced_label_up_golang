393ca33cedcb29b1b491e702d438249aea45e59e
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-03-28 15:36:22 +0600

    absulate,base Path

---
+++ 22_file_handling/03_path_handling_base_relative_aubsulate/main/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	// dir, err := os.Getwd()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(dir)
+	// err = os.Mkdir("../filemk", 0777)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//base path
+	// basePATH := filepath.Base(dir)
+	// fmt.Println(basePATH)
+	// //relative path
+	// relativePATH := filepath.Join("../filemk")
+	// fmt.Println(relativePATH)
+	//fullpath
+	absulatePATH, _ := filepath.Abs("../filemk")
+	// fmt.Println(absulatePATH)
+	//make another folder
+	nePATH := filepath.Join(absulatePATH, "../", "../", "04_file")
+	err := os.Mkdir(nePATH, 777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println(nePATH)
+
+}
