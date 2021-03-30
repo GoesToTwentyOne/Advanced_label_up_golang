b46cc88461d5a57547200355870de8db46466131
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-03-30 12:00:02 +0600

    big problem solve

--- 22_file_handling/04_excel_file_handling/main.go
+++ 22_file_handling/04_excel_file_handling/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/360EntSecGroup-Skylar/excelize/v2"
 )
 
 func main() {
@@ -11,16 +11,16 @@ func main() {
 	// Create a new sheet.
 	index := f.NewSheet("Sheet2")
 	// Set value of a cell.
-	f.SetCellValue("Sheet2", "A2", "Hello world.")
-	f.SetCellValue("Sheet1", "B2", 100)
-	f.SetCellValue("Sheet2", "A5", "Myname is Nihad.")
-	f.SetCellValue("Sheet1", "B5", 300)
+	f.SetCellValue("Sheet2", "A2", "Hello world Bangladesh.")
+	f.SetCellValue("Sheet1", "B2", 200)
+	f.SetCellValue("Sheet2", "A5", "Myname Rowjatul.")
+	f.SetCellValue("Sheet1", "B5", 400)
 	// Set active sheet of the workbook.
 	f.SetActiveSheet(index)
 	// Save spreadsheet by the given path.
 	if err := f.SaveAs("test.xlsx"); err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Hello this is run")
+	fmt.Println("Hello runnnin and running")
 
 }
